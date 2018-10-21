efbf527abd97320ff72bb85debccbadd7f9cd40a
Akshay Nair <phenax5@gmail.com>
2018-10-21 21:17:59 +0530

    Moar refactoring

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,12 @@
 
+// compose :: (...Function) -> Function
+export const compose = (...fns) =>
+    fns.reduce((a, b) => (...args) => a(b(...args)));
+// map :: (a -> b) -> [a] -> [b]
+export const map = fn => arr => arr.map(fn);
+// filter :: (a -> Bool) -> [a] -> [a]
+export const filter = fn => arr => arr.filter(fn);
+
 // fromPairs :: [[String, b]] -> Object b
 export const fromPairs = pairs =>
     pairs.reduce((acc, [ k, v ]) => ({ ...acc, [k]: v }), {});
@@ -9,20 +17,16 @@ export const createMethod = method =>
         obj[method].apply(obj, args);
 
 // isFunction :: * -> Bool
-const isFunction = fn => fn instanceof Function
+const isFunction = fn => fn && fn instanceof Function;
+
+const isMethod = Class => key =>
+    key !== 'constructor' && isFunction(Class.prototype[key]);
 
 // getMethods :: Class -> [String]
 export const getMethods = Class =>
     !Class || !Class.prototype
         ? []
-        : Object.getOwnPropertyNames(Class.prototype)
-            .filter(key => key !== 'constructor')
-            .filter(key => isFunction(Class.prototype[key]));
-
-// compose :: (...Function) -> Function
-export const compose = (...fns) =>
-    fns.reduce((a, b) => (...args) => a(b(...args)));
-// map :: (a -> b) -> [a] -> [b]
-export const map = fn => arr => arr.map(fn);
-// filter :: (a -> Bool) -> [a] -> [a]
-export const filter = fn => arr => arr.filter(fn);
+        : compose(
+            filter(isMethod(Class)),
+            Object.getOwnPropertyNames,
+        )(Class.prototype);
